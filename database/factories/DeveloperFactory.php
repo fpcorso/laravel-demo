1eb4334a4215f73ddf0c8f388820d2ae8d51f649
Frank Corso <frank@frankcorso.me>
2023-11-08 10:20:19 -0500

    Add developer model

--- database/factories/DeveloperFactory.php
+++ database/factories/DeveloperFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Game>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Developer>
  */
-class GameFactory extends Factory
+class DeveloperFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,11 +17,10 @@ class GameFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->words(3, true),
+            'name' => $this->faker->company(),
+            'founded' => $this->faker->year(),
             'description' => $this->faker->paragraphs(3, true),
-            'release_date' => $this->faker->date(),
-            'publisher_id' => \App\Models\Publisher::factory(),
-            'developer_id' => \App\Models\Developer::factory(),
+            'website' => $this->faker->url(),
         ];
     }
 }
