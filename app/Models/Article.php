8e0675a578b3bbf58f55417a84653c600cb0e4a7
Frank Corso <frank@frankcorso.me>
2023-11-10 12:08:38 -0500

    Add relationship between feeds and articles

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * Get the feed that owns the article.
+     */
+    public function feed(): BelongsTo
+    {
+        return $this->belongsTo(Feed::class);
+    }
 }
--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Feed extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * Get the articles for the feed.
+     */
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
 }
